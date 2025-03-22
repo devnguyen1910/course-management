3c086b1691bb0fb223192735f10778b3b07fc133
Tuong Nguyen Minh <tuongnguyen191003@gmail.com>
2025-03-22 17:40:19 +0700

    Tuong | Completed Task: SH_MN | 22/03

---
+++ DigComp/Controllers/CourseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigComp.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //============================SH_MN_005i - TRANG WISHLIST KHÓA HỌC===================================================================================================
+        public IActionResult WLCourse()
+        {
+            return View();
+        }
+    }
+}
---
+++ DigComp/Controllers/ServiceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigComp.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //============================SH_MN_005v - TRANG WISHLIST SẢN PHẨM DỊCH VỤ===================================================================================================
+        public IActionResult WLService()
+        {
+            return View();
+        }
+    }
+}
