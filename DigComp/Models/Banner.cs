40b695929751c39fde8ceaecacbb233f08c99ecd
Phuctai1672003 <136880259+Phuctai1672003@users.noreply.github.com>
2025-03-23 20:46:04 +0700

    Banner+why chosse us

---
+++ DigComp/Models/Banner.cs
@@ -0,0 +1,10 @@
+﻿namespace DigComp.Models
+{
+    public class Banner
+    {
+        public int Id { get; set; } // Thêm Id nếu bạn cần
+        public string ImageUrl { get; set; }
+        public bool IsActive { get; set; }
+        public string LinkUrl { get; set; }
+    }
+}
